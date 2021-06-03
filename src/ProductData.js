a823ce8c8f3ee25e3be856be4ade9d6ffdb9a187
Randi Reyna <77865204+randi09@users.noreply.github.com>
2021-06-03 13:41:15 -0500

    added image to product data

--- src/ProductData.js
+++ src/ProductData.js
@@ -1,87 +1,105 @@
 const ProductData = [
-    {
-        id: "28100",
-        name: "Wood Grain Tumbler",
-        desription: "Wood Grain Tumbler with American Flag and Deer, Mens Tumbler, Tumbler for Men, Hunting Tumbler, Personalized Tumbler",
-        price: 35.00,
-        image: <img src=""
-    }
-    {
-        id: "28101",
-        name: "Marble Tumbler",
-        desription: "Rose Gold Marble, Light Rose Gold Glitter Tumbler",
-        price: 35.00
-    }
-    {
-        id: "28102",
-        name: "Galaxy Tumbler",
-        desription: "Astronaut tumbler, Space Tumbler, Universe, Galaxy Glitter Tumbler, Planet Tumbler, Personalized Tumbler",
-        price: 35.00
-    }
-    {
-        id: "28103",
-        name: "Texans Tumbler",
-        desription: "Houston Texans Tumbler. We can add name or customize to your preference. Colors gold and silver with vinyl design and epoxy coat.",
-        price: 45.00
-    }
-    {
-        id: "28104",
-        name: "Fishing Tumbler",
-        desription: "Fishing Flag Wood | American Flag | Skinny Tumbler Wrap",
-        price: 30.00
-    }
-    {
-        id: "28105",
-        name: "Harry Potter Tumbler",
-        desription: "Harry Potter Glitter Tumbler | Harry Potter Burned Book Pages Tumbler",
-        price: 45.00
-    }
-    {
-        id: "28106",
-        name: "Casino Tumbler",
-        desription: "Casino Gambler Epoxy Stainless Steel Tumbler",
-        price: 40.00
-    }
-    {
-        id: "28107",
-        name: "Smoke Effect Tumbler",
-        desription: "Smoke Effect Tumbler Stainless Steel",
-        price: 30.00
-    }
-    {
-        id: "28108",
-        name: "Tattoo Tumbler",
-        desription: "Skull Tattoo Tumbler",
-        price: 25.00
-    }
-    {
-        id: "28109",
-        name: "Water Slide Sunflower Tumbler",
-        desription: "Personalized Stainless Steel Sunflower tumbler",
-        price: 35.00
-    }
-    {
-        id: "28110",
-        name: "Unicorn Tumbler",
-        desription: "Stainless Steel Unicorn Glitter Tumbler",
-        price: 30.00
-    }
-    {
-        id: "28111",
-        name: "Crime Scene Tumbler",
-        desription: "Customized Crime Scene Stainless Steel Tumbler",
-        price: 35.00
-    }
-    {
-        id: "28112",
-        name: "Butterfly Tumbler",
-        desription: "Customized Butterfly Stainless Steel Glitter Tumbler",
-        price: 30.00
-    }
-    {
-        id: "28113",
-        name: "Grinch Theme Tumbler",
-        desription: "Glitter tumbler grinch glitter tumbler handmade tumbler stainless steel glitter dipped travel coffee mug Christmas tumbler",
-        price: 40.00
-    }
+  {
+    id: "28100",
+    name: "Wood Grain Tumbler",
+    desription:
+      "Wood Grain Tumbler with American Flag and Deer, Mens Tumbler, Tumbler for Men, Hunting Tumbler, Personalized Tumbler",
+    price: 35.0,
+    image: "../images/WoodGrainTumbler",
+  },
+  {
+    id: "28101",
+    name: "Marble Tumbler",
+    desription: "Rose Gold Marble, Light Rose Gold Glitter Tumbler",
+    price: 35.0,
+    image: "",
+  },
+  {
+    id: "28102",
+    name: "Galaxy Tumbler",
+    desription:
+      "Astronaut tumbler, Space Tumbler, Universe, Galaxy Glitter Tumbler, Planet Tumbler, Personalized Tumbler",
+    price: 35.0,
+    image: "",
+  },
+  {
+    id: "28103",
+    name: "Texans Tumbler",
+    desription:
+      "Houston Texans Tumbler. We can add name or customize to your preference. Colors gold and silver with vinyl design and epoxy coat.",
+    price: 45.0,
+    image: "",
+  },
+  {
+    id: "28104",
+    name: "Fishing Tumbler",
+    desription: "Fishing Flag Wood | American Flag | Skinny Tumbler Wrap",
+    price: 30.0,
+    image: "",
+  },
+  {
+    id: "28105",
+    name: "Harry Potter Tumbler",
+    desription:
+      "Harry Potter Glitter Tumbler | Harry Potter Burned Book Pages Tumbler",
+    price: 45.0,
+    image: "",
+  },
+  {
+    id: "28106",
+    name: "Casino Tumbler",
+    desription: "Casino Gambler Epoxy Stainless Steel Tumbler",
+    price: 45.0,
+    image: "",
+  },
+  {
+    id: "28107",
+    name: "Smoke Effect Tumbler",
+    desription: "Smoke Effect Tumbler Stainless Steel",
+    price: 30.0,
+    image: "",
+  },
+  {
+    id: "28108",
+    name: "Tattoo Tumbler",
+    desription: "Skull Tattoo Tumbler",
+    price: 25.0,
+    image: "",
+  },
+  {
+    id: "28109",
+    name: "Water Slide Sunflower Tumbler",
+    desription: "Personalized Stainless Steel Sunflower tumbler",
+    price: 35.0,
+    image: "",
+  },
+  {
+    id: "28110",
+    name: "Unicorn Tumbler",
+    desription: "Stainless Steel Unicorn Glitter Tumbler",
+    price: 30.0,
+    image: "",
+  },
+  {
+    id: "28111",
+    name: "Crime Scene Tumbler",
+    desription: "Customized Crime Scene Stainless Steel Tumbler",
+    price: 35.0,
+    image: "",
+  },
+  {
+    id: "28112",
+    name: "Butterfly Tumbler",
+    desription: "Customized Butterfly Stainless Steel Glitter Tumbler",
+    price: 30.0,
+    image: "",
+  },
+  {
+    id: "28113",
+    name: "Grinch Theme Tumbler",
+    desription:
+      "Glitter tumbler grinch glitter tumbler handmade tumbler stainless steel glitter dipped travel coffee mug Christmas tumbler",
+    price: 40.0,
+    image: "",
+  },
 ];
